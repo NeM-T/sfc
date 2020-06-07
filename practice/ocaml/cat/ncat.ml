c1b81b20fff35cc04e23157acb37fcd140250bf1
NeM-T <persodou@keio.jp>
2020-06-07 23:14:05 +0900

    Unit型のほうが正しい気がする

--- practice/ocaml/cat/ncat.ml
+++ practice/ocaml/cat/ncat.ml
@@ -8,7 +8,7 @@ let buf = Bytes.create 1024
 let rec wr fd bf =
   let r = read fd bf 0 1024 in
   match r with
-  | 0 -> None
+  | 0 -> ()
   | _ ->
       let _ = write stdout bf 0 r in
       wr fd bf
@@ -17,9 +17,9 @@ let rec wr fd bf =
 let rec rwwhile n  =
   let file = Sys.argv.(argc - n)  in
   let fd = openfile file [O_RDONLY] 0o640 in
-  let _ = wr fd buf in
+  let () = wr fd buf in
   let () = close fd in
-  if n <= 1 then None else
+  if n <= 1 then () else
       rwwhile (n - 1)
 
-let _ = rwwhile (argc - 1)
+let () = rwwhile (argc - 1)
